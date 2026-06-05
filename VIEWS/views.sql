f7237700b3d8a63fa0a0a0ce7f55968064709f6b
Nandini Reddy <nandureddy770@gmail.com>
2026-06-05 20:42:08 +0530

    Update views.sql

--- VIEWS/views.sql
+++ VIEWS/views.sql
@@ -1,1 +1,29 @@
 
+views:-
+
+What is a View?
+
+A View is a virtual table created from a SQL query.
+It does not store data, it only shows data from real tables.
+Why Views?
+Hide sensitive data (security)
+Simplify complex queries
+Reusable SQL logic
+
+SELECT * FROM employee;
+
+CREATE VIEW emp_basic AS
+SELECT employee_id, name, department
+FROM employee;
+
+CREATE VIEW emp_basic AS
+SELECT emp_id, emp_name
+FROM employee;
+
+select * from emp_basic;
+
+select * from high_salary_emp;
+CREATE VIEW high_salary_emp AS
+SELECT *
+FROM employee
+WHERE salary > 30000;
