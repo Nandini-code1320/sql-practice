f546ee3f30eee98181b7e46c5601a121a99fd81b
Nandini Reddy <nandureddy770@gmail.com>
2026-06-05 21:31:58 +0530

    Update notes.sql

--- INDEX/notes.sql
+++ INDEX/notes.sql
@@ -1,1 +1,17 @@
 
+What is an Index?
+
+An Index is like a book index 📖
+It helps database find data faster without scanning full table.
+
+CREATE INDEX index_name
+ON table_name(column_name);
+
+CREATE INDEX idx_emp_name
+ON employee(emp_name);
+
+CREATE INDEX idx_salary
+ON employee(salary);
+DROP INDEX idx_emp_name;
+
+SELECT * FROM employee WHERE emp_name = 'nandu';
