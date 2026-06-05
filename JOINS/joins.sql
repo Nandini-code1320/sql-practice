3d969595eea7266edd9a11ab4cf4f501383b0a84
Nandini Reddy <nandureddy770@gmail.com>
2026-06-05 19:51:57 +0530

    Added JOINS practice queries

--- JOINS/joins.sql
+++ JOINS/joins.sql
@@ -1,1 +1,34 @@
 
+joins:-👉 JOIN = used to combine data from two tables
+
+You already have:
+
+employee
+department
+
+SELECT e.emp_name, e.salary, d.dept_name
+FROM nandu.employee e
+INNER JOIN nandu.department d
+ON e.dept_id = d.dept_id;
+
+
+SELECT e.emp_name, d.dept_name
+FROM nandu.employee e
+LEFT JOIN nandu.department d
+ON e.dept_id = d.dept_id;
+
+SELECT e.emp_name, d.dept_name
+FROM nandu.employee e
+RIGHT JOIN nandu.department d
+ON e.dept_id = d.dept_id;
+
+SELECT e.emp_name, d.dept_name
+FROM nandu.employee e
+FULL OUTER JOIN nandu.department d
+ON e.dept_id = d.dept_id;
+
+SELECT a.emp_name AS Employee1,
+       b.emp_name AS Employee2
+FROM nandu.employee a
+JOIN nandu.employee b
+ON a.dept_id = b.dept_id;
